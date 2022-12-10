8d5ffe4aee59d90dc1a47f2b7edb1b28919b56ee
Hazem Elakbawy <hazemelakbawy62@gmail.com>
2022-12-10 21:27:00 +0200

    reformatting files

--- main.py
+++ main.py
@@ -2,79 +2,82 @@ from unordered_list import UnorderedList
 from random import randrange
 from time import time
 
+
 def random_linked_list(n):
-  """ Generate random linked list that has the length of n """
-  alist = UnorderedList()
-  for i in range(n):
-    alist.add(randrange(0, 101))
-  return alist
+    """ Generate random linked list that has the length of n """
+    alist = UnorderedList()
+    for i in range(n):
+        alist.add(randrange(0, 101))
+    return alist
+
 
 def linked_list_selection_sort(alist):
-  """ Operate selection sort on a linked list """
-  length = alist.getLength()
-  pointer = alist.getHead()
+    """ Operate selection sort on a linked list """
+    length = alist.getLength()
+    pointer = alist.getHead()
 
-  for i in range(length-1):
-    mn = pointer
-    cursor = pointer.getNext()
-    while cursor is not None:
-      if cursor.getData() < mn.getData():
-        mn = cursor
-      cursor = cursor.getNext()
+    for i in range(length - 1):
+        mn = pointer
+        cursor = pointer.getNext()
+        while cursor is not None:
+            if cursor.getData() < mn.getData():
+                mn = cursor
+            cursor = cursor.getNext()
 
-    # Swaping the elements
-    temp = mn.getData()
-    mn.setData(pointer.getData())
-    pointer.setData(temp)
-  
-    pointer = pointer.getNext()
+        # Swaping the elements
+        temp = mn.getData()
+        mn.setData(pointer.getData())
+        pointer.setData(temp)
+
+        pointer = pointer.getNext()
+
 
 def generate_normal_list(n):
-  """ Generate random normal list that has the length of n """
-  alist = []
-  for i in range(n):
-    alist += [randrange(0, 101)]
-  return alist
+    """ Generate random normal list that has the length of n """
+    alist = []
+    for i in range(n):
+        alist += [randrange(0, 101)]
+    return alist
+
 
 def normal_list_selection_sort(alist):
-  """ Operate selection sort on a normal list """
-  for i in range(len(alist)-1):
-    min_value = alist[i]
-    min_index = i
+    """ Operate selection sort on a normal list """
+    for i in range(len(alist) - 1):
+        min_value = alist[i]
+        min_index = i
 
-    for j in range(i, len(alist)):
-      if alist[j] < min_value:
-        min_value = alist[j]
-        min_index = j
+        for j in range(i, len(alist)):
+            if alist[j] < min_value:
+                min_value = alist[j]
+                min_index = j
 
-    alist[min_index], alist[i] = alist[i], alist[min_index]
+        alist[min_index], alist[i] = alist[i], alist[min_index]
+
 
 if __name__ == "__main__":
+    # Generate random linked list
+    linked_list = random_linked_list(100)
 
-  # Generate random linked list
-  linked_list = random_linked_list(100)
+    print("List before sorting:")
+    linked_list.printList()
+    print()
+    begin_time = time()
 
-  print("List before sorting:")
-  linked_list.printList()
-  print()
-  begin_time = time()
-  
-  # Sorting operation
-  linked_list_selection_sort(linked_list)
-  end_time = time()
+    # Sorting operation
+    linked_list_selection_sort(linked_list)
+    end_time = time()
 
-  print("List after sorting:")
-  linked_list.printList()
-  print()
+    print("List after sorting:")
+    linked_list.printList()
+    print()
 
-  # Calculating time of the operation
-  print(f"Total time of linked list selection sort: {end_time - begin_time:.9f} sec")
+    # Calculating time of the operation
+    print(f"Total time of linked list selection sort: {end_time - begin_time:.9f} sec")
 
-  # Doing the same operation on normal list
-  normal_list = generate_normal_list(100)
-  begin_time = time()
-  normal_list_selection_sort(normal_list)
-  end_time = time()
+    # Doing the same operation on normal list
+    normal_list = generate_normal_list(100)
+    begin_time = time()
+    normal_list_selection_sort(normal_list)
+    end_time = time()
 
-  print(f"Total time of normal list selection sort: {end_time - begin_time:.9f} sec")
-  
+    print(f"Total time of normal list selection sort: {end_time - begin_time:.9f} sec")
--- node.py
+++ node.py
@@ -1,25 +1,23 @@
 class Node:
-  """ Node Class: used to represent data structures like linked list, tree, ... etc """
+    """ Node Class: used to represent data structures like linked list, tree, ... etc """
 
-  def __init__(self, initData):
-    """ Construct node with value and set next to none """
-    self.data = initData
-    self.next = None
+    def __init__(self, initData):
+        """ Construct node with value and set next to none """
+        self.data = initData
+        self.next = None
 
-  def getData(self):
-    """ Returns the value of the node """
-    return self.data
+    def getData(self):
+        """ Returns the value of the node """
+        return self.data
 
-  def getNext(self):
-    """ Returns the pointer that refer to the next node """
-    return self.next
+    def getNext(self):
+        """ Returns the pointer that refer to the next node """
+        return self.next
 
-  def setData(self, newData):
-    """ Changes the value of the node data """
-    self.data = newData
+    def setData(self, newData):
+        """ Changes the value of the node data """
+        self.data = newData
 
-
-  def setNext(self, newNext):
-    """ Changes what the node refrence for """
-    self.next = newNext
-
+    def setNext(self, newNext):
+        """ Changes what the node refrence for """
+        self.next = newNext
